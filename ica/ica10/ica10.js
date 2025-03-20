812f04d8411d9ee77f1b5ed12768378e37035356
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2025-03-19 18:44:10 -0600

    random color button added

--- ica/ica10/ica10.js
+++ ica/ica10/ica10.js
@@ -1,63 +1,73 @@
-let btn = document.getElementById('button'); //document.querySelector('button'); 
+let runBtn = document.getElementById('runButton'); //document.querySelector('button'); 
+let colBtn = document.getElementById('colorButton');
 
-let xPos = window.innerWidth / 2 - btn.offsetWidth / 2;
-let yPos = window.innerHeight / 2 - btn.offsetHeight / 2;
+let xPos = window.innerWidth / 2 - runBtn.offsetWidth / 2;
+let yPos = window.innerHeight / 2 - runBtn.offsetHeight / 2;
     
-btn.style.left = xPos + 'px';
-btn.style.top = yPos + 'px';
+runBtn.style.left = xPos + 'px';
+runBtn.style.top = yPos + 'px';
 
-btn.addEventListener('mouseover', runAway);
-btn.addEventListener('click', clicked);
+runBtn.addEventListener('mouseover', runAway);
+runBtn.addEventListener('click', clicked);
+colBtn.addEventListener('click', changeColor);
+
+function changeColor(){
+    const newColor = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+    const elems = document.querySelectorAll('.colorChange');
+
+    for(const elem of elems){
+        elem.style.color = newColor;
+    }
+}
 
 function runAway(){
-    xPos = btn.offsetLeft;
-    yPos = btn.offsetTop;
+    xPos = runBtn.offsetLeft;
+    yPos = runBtn.offsetTop;
     // let newHeight = btn.offsetHeight > 20? btn.offsetHeight - 5 : 20;
     // let newWidth = btn.offsetWidth > 80? btn.offsetWidth - 20 : 80;
     let newX = xPos;
     let newY = yPos;
     let num = Math.random();
     
-    if(num > 0.5){
-        newX += (num * 50);
+    if(num > 0.65){
+        newX += (num * 100);
 
-        if(newX + btn.offsetWidth > window.innerWidth){
-            newX -= ((num * 50)*2); 
+        if(newX + runBtn.offsetWidth > window.innerWidth){
+            newX -= ((num * 100)*2); 
         }
     }
     else{
-        newX -= (num * 50);
+        newX -= (num * 100);
 
         if(newX < 0){
-            newX (num * 100);
+            newX += ((num * 100) * 2);
         }
     }
 
-    if(num < 0.5){
-        newY += 20;
+    if(num < 0.65){
+        newY += (num * 100);
 
-        if(newY + btn.offsetHeight > window.innerHeight){
-            newY -= 40; 
+        if(newY + runBtn.offsetHeight > window.innerHeight){
+            newY -= ((num * 100)*2); 
         }
     }
     else{
-        newY -= 20;
+        newY -= (num * 100);
 
         if(newY < 0){
-            newY += 40;
+            newY += ((num * 100)*2);
         }
     }
-    
 
-    btn.style.left = newX + "px";
-    btn.style.top = newY + "px";
+    runBtn.style.left = newX + "px";
+    runBtn.style.top = newY + "px";
     // btn.style.height = newHeight + "px";
     // btn.style.width = newWidth + "px";
 }
 
 
 function clicked(){
-    alert('YOU CLICKED TO BUTTON!');
+    alert('YOU CLICKED THE BUTTON!');
     location.reload();
 
     //change size of button and its text
