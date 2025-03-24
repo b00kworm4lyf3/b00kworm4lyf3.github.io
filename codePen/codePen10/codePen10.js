632c4eea1771d1efc27b1f284a845e92b62c8f2c
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2025-03-24 15:57:07 -0600

    pls work now ommg

--- codePen/codePen10/codePen10.js
+++ codePen/codePen10/codePen10.js
@@ -1,4 +1,4 @@
-import * as THREE from "/node_modules/three/build/three.module.js";
+import * as THREE from "/codePen/codePen10/node_modules/three/build/three.module.js";
 import { EffectComposer } from '/codePen/codePen10/node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from '/codePen/codePen10/node_modules/three/examples/jsm/postprocessing/RenderPass.js';
 import { ShaderPass } from '/codePen/codePen10/node_modules/three/examples/jsm/postprocessing/ShaderPass.js';
