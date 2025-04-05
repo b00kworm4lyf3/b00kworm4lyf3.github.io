ed349ca1788737b3f6e48d4333b2df45e9ddd0ae
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2025-04-05 12:07:28 -0600

    grid background done

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const images = ['vegas1.jpg', 'vegas2.jpg', 'vegas3.jpg', 'vegas4.jpg', 'vegas5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const altTxt = ['Closeup of a human eye', 'Closeup of a rock', 'Closeup of some flowers', 'Egyptian painting', 'A butterfly with its wings open'];
