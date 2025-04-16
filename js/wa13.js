af819949e4812fa888c5f1704be24180e9a1374e
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2025-04-16 13:39:45 -0600

    updated to functional approach, no change in output

--- js/wa13.js
+++ js/wa13.js
@@ -37,44 +37,58 @@ const compJSON = {
 console.log("2) Company JSON created: ", compJSON);
 
 //Problem 3
-const newEmp = {
-    firstName: "Anna",
-    dept: "Tech",
-    desig: "Executive",
-    salary: 25600,
-    raiseElig: false,
+function addNewEmployee(compJSON, firstName, dept, desig, salary, raiseElig){
+    const newEmp = {
+        firstName: firstName,
+        dept: dept,
+        desig: desig,
+        salary: salary,
+        raiseElig: raiseElig,
+    };
+
+    compJSON.employees.push(newEmp);
 };
 
-compJSON.employees.push(newEmp);
+addNewEmployee(compJSON, "Anna", "Tech", "Executive", 25600, false);
 console.log("3) New employee added to both JSONs: ", empJSON, compJSON);
 
 //Problem 4
-let totSal = 0;
+function getTotSal(compJSON){
+    let totSal = 0;
 
-for(let i = 0; i < compJSON.employees.length; i++){
-    totSal += compJSON.employees[i].salary;
-}
-console.log("4) Total employee salaries: ", totSal);
+    for(let i = 0; i < compJSON.employees.length; i++){
+        totSal += compJSON.employees[i].salary;
+    }
+    return totSal;
+};
+
+console.log("4) Total employee salaries: ", getTotSal(compJSON));
 
 //Problem 5
-for(let i = 0; i < compJSON.employees.length; i++){
-    if(compJSON.employees[i].raiseElig){
-        compJSON.employees[i].salary *= 1.1;
-        compJSON.employees[i].raiseElig = false;
+function raiseTime(compJSON){
+    for(let i = 0; i < compJSON.employees.length; i++){
+        if(compJSON.employees[i].raiseElig){
+            compJSON.employees[i].salary *= 1.1;
+            compJSON.employees[i].raiseElig = false;
+        }
     }
-}
+};
 
+raiseTime(compJSON);
 console.log("5) Eligible employees given raises: ", compJSON);
 
 //Problem 6
-compJSON.wfh = ['Anna', 'Sam'];
-for(let i = 0; i < compJSON.employees.length; i++){
-    if(compJSON.wfh.includes(compJSON.employees[i].firstName)){
-        compJSON.employees[i].wfh = true;
+function addWFH(compJSON, empNames){
+    compJSON.wfh = empNames;
+    for(let i = 0; i < compJSON.employees.length; i++){
+        if(compJSON.wfh.includes(compJSON.employees[i].firstName)){
+            compJSON.employees[i].wfh = true;
+        }
+        else{
+            compJSON.employees[i].wfh = false;
+        }
     }
-    else{
-        compJSON.employees[i].wfh = false;
-    }
-}
+};
 
+addWFH(compJSON, ['Sam', 'Anna']);
 console.log("6) Employees updated with WFH status: ", compJSON);
